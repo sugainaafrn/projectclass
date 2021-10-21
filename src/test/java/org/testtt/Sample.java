e5fe2b6f006464e478fddbe580204030c76ffbe0
sugainaafrn <afreensugaina@gmail.com>
2021-10-21 23:57:43 +0530

    Conflicts Resolved Code

--- src/test/java/org/testtt/Sample.java
+++ src/test/java/org/testtt/Sample.java
@@ -3,10 +3,11 @@ package org.testtt;
 public class Sample {
 
 	public static void main(String[] args) {
+		
+		System.out.println("Test1");
+		System.out.println("Test2");
 		System.out.println("test3");
 		System.out.println("test4");
-		
-	System.out.println("Test1");
-	System.out.println("Test2");
+	
   }
  }
